ab3315cc3cad41fcdeab8cb80eafe08348c7d1b8
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-25 12:32:01 -0300

    build: creating principal apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export default () => {
+    const [amount, setAmount] = useState(0);
+
+    const makeMoney = (money: number) => {
+        setAmount((amount) => amount + money);
+    };
+
+    const setTimer = (time: Number) => {
+        return time;
+    };
+
     return (
         <>
-            <h1>Texto aqui</h1>
+            <button onClick={() => makeMoney(1)}>Farm</button>
+            <h1>{amount}</h1>
+            <div onClick={() => makeMoney(15)}>
+                npc-make-money +15 a cada 5s
+            </div>
         </>
     );
 };
