7f1949abf9c9bac30569ff54202af8c9cfe8f151
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-09-26 13:07:51 -0300

    build: add sprites and creating game design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import Dashboard from "./components/Dashboard.tsx";
 import { useEffect, useState } from "react";
 
+import Shovel from "./assets/sprites-npc/shovel-idle.png";
+import ShovelTake from "./assets/sprites-npc/shovel-move.png";
+
 export default () => {
     const [amount, setAmount] = useState(0);
+    const [idle, setIdle] = useState(Shovel);
+
+    const changeAnimation = (idleSprite: string, moveSprite: string) => {
+        setIdle(moveSprite);
+        setInterval(() => {
+            setIdle(idleSprite);
+        }, 500);
+    };
+
+    const digMoney = () => {
+        changeAnimation(Shovel, ShovelTake);
+        makeMoney(1);
+    };
 
     const makeMoney = (money: number) => {
         setAmount((amount) => amount + money);
@@ -23,10 +39,15 @@ export default () => {
     return (
         <>
             <Dashboard />
-            <button onClick={() => makeMoney(1)}>Farm</button>
-            <h1>R${amount}</h1>
-            <div onClick={() => buyCompany(amount)}>
-                npc-make-money +15 a cada 5s
+            <div>
+                <img src={idle} alt="img-npc" />
+                <button onClick={() => digMoney()}>CAVAR</button>
+                <h1>R${amount}</h1>
+            </div>
+            <div>
+                <div onClick={() => buyCompany(amount)}>
+                    npc-make-money +15 a cada 5s
+                </div>
             </div>
         </>
     );
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,20 +1,26 @@
+import { useState } from "react";
+import Miner from "../assets/sprites-npc/miner.png";
+import Brock from "../assets/sprites-npc/brock.png";
+
 export default () => {
+    const [hideShop, setHideShop] = useState(true);
+
     return (
         <>
             <header className="dashboard-header">
                 <nav className="dash-title">
                     <h1>Loja</h1>
-                    <button>pa baixo</button>
+                    <button onClick={() => setHideShop(false)}>vvv</button>
                 </nav>
-                <ul className="dashboard-list">
+                <ul className={`dashboard-list ${hideShop && "d-none"}`}>
                     <li className="item-dash">
-                        <img src="https://fakeimg.pl/70x70/" alt="item" />
+                        <img src={Miner} alt="item" />
                         <div>
                             <button className="btn-dash">50$</button>
                         </div>
                     </li>
                     <li className="item-dash">
-                        <img src="https://fakeimg.pl/70x70/" alt="item" />
+                        <img src={Brock} alt="item" />
                         <div>
                             <button className="btn-dash">50$</button>
                         </div>
